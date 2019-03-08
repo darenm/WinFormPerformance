4babd752a1c06449914fb83fb0c9c189dbdb6b83
Daren May <daren.may@gmail.com>
2019-03-08 09:03:43 -0700

    Add project files.

---
+++ PieChartTestFW/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PieChartTestFW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            DrawPieChartLoop(10);
+        }
+
+        private void DrawPieChartLoop(int loopCount)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            for (int j = 0; j < loopCount; j++)
+            {
+                using (var graphics = this.CreateGraphics())
+                {
+                    graphics.Clear(Color.White);
+                    for (int i = 0; i < 200; i++)
+                    {
+                        DrawPieChartOnForm(graphics, new Point(10 + i, 10), new Size(150, 150));
+                    }
+                }
+            }
+
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            renderResults.Text = elapsedTime;
+        }
+        public void DrawPieChartOnForm(Graphics myPieGraphic, Point myPieLocation, Size myPieSize)
+        {
+            //Take Total Five Values & Draw Chart Of These Values.
+            int[] myPiePercent = { 10, 20, 25, 5, 40 };
+
+            //Take Colors To Display Pie In That Colors Of Taken Five Values.
+            Color[] myPieColors = { Color.Red, Color.Black, Color.Blue, Color.Green, Color.Maroon };
+
+            //Call Function Which Will Draw Pie of Values.
+            DrawPieChart(myPiePercent, myPieColors, myPieGraphic, myPieLocation, myPieSize);
+        }
+
+
+        // Draws a pie chart.
+        public void DrawPieChart(int[] myPiePerecents, Color[] myPieColors, Graphics myPieGraphic, Point
+      myPieLocation, Size myPieSize)
+        {
+            //Check if sections add up to 100.
+            int sum = 0;
+            foreach (int percent_loopVariable in myPiePerecents)
+            {
+                sum += percent_loopVariable;
+            }
+
+            if (sum != 100)
+            {
+                MessageBox.Show("Sum Do Not Add Up To 100.");
+            }
+
+            //Check Here Number Of Values & Colors Are Same Or Not.They Must Be Same.
+            if (myPiePerecents.Length != myPieColors.Length)
+            {
+                MessageBox.Show("There Must Be The Same Number Of Percents And Colors.");
+            }
+
+            int PiePercentTotal = 0;
+            for (int PiePercents = 0; PiePercents < myPiePerecents.Length; PiePercents++)
+            {
+                using (SolidBrush brush = new SolidBrush(myPieColors[PiePercents]))
+                {
+
+                    //Here it Will Convert Each Value Into 360, So Total Into 360 & Then It Will Draw A Full Pie Chart.
+                    myPieGraphic.FillPie(brush, new Rectangle(myPieLocation, myPieSize), Convert.ToSingle(PiePercentTotal * 360 / 100), Convert.ToSingle(myPiePerecents[PiePercents] * 360 / 100));
+                }
+
+                PiePercentTotal += myPiePerecents[PiePercents];
+            }
+            return;
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.renderResults = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // renderResults
+            // 
+            this.renderResults.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.renderResults.AutoSize = true;
+            this.renderResults.Location = new System.Drawing.Point(622, 1487);
+            this.renderResults.Name = "renderResults";
+            this.renderResults.Size = new System.Drawing.Size(72, 25);
+            this.renderResults.TabIndex = 5;
+            this.renderResults.Text = "not run";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(295, 1488);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(320, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Time to render 10 x 200 Pie Charts:";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(12, 1478);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(277, 45);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Draw Charts";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1491, 1535);
+            this.Controls.Add(this.renderResults);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label renderResults;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
